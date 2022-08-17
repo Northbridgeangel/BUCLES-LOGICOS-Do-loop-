d656e1b858f723fb2114ad506e8b2db4a77995a0
Northbridgeangel <67518040+Northbridgeangel@users.noreply.github.com>
2022-08-17 09:09:54 +0200

    Agregar archivos de proyecto.

---
+++ Form1.Designer.vb
@@ -0,0 +1,254 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox4 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox5 = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.ComboBox6 = New System.Windows.Forms.ComboBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 36)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(226, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "REPITE HASTA NO (LOOP UNTIL)"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 65)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(226, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "REPITE HASTA NO (DO WHILE)"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(12, 94)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(226, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "REPITE HASTA NO (WHILE-END WHILE)"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(15, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(223, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "3 METODOS DE HACER BUCLES LÓGICOS"
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(12, 146)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(226, 23)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "REPITE HASTA METER UN PAR"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(350, 12)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(226, 23)
+        Me.Button5.TabIndex = 5
+        Me.Button5.Text = "MATRIZ"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(582, 14)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(206, 21)
+        Me.ComboBox1.TabIndex = 6
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(350, 94)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(226, 23)
+        Me.Button6.TabIndex = 7
+        Me.Button6.Text = "METE NUMEROS EN EL VECTOR"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(582, 94)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(206, 21)
+        Me.ComboBox2.Sorted = True
+        Me.ComboBox2.TabIndex = 8
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(696, 158)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(67, 23)
+        Me.Button7.TabIndex = 9
+        Me.Button7.Text = "INICIO"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Location = New System.Drawing.Point(34, 232)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(206, 21)
+        Me.ComboBox3.Sorted = True
+        Me.ComboBox3.TabIndex = 10
+        '
+        'ComboBox4
+        '
+        Me.ComboBox4.FormattingEnabled = True
+        Me.ComboBox4.Location = New System.Drawing.Point(299, 232)
+        Me.ComboBox4.Name = "ComboBox4"
+        Me.ComboBox4.Size = New System.Drawing.Size(206, 21)
+        Me.ComboBox4.Sorted = True
+        Me.ComboBox4.TabIndex = 11
+        '
+        'ComboBox5
+        '
+        Me.ComboBox5.FormattingEnabled = True
+        Me.ComboBox5.Location = New System.Drawing.Point(557, 232)
+        Me.ComboBox5.Name = "ComboBox5"
+        Me.ComboBox5.Size = New System.Drawing.Size(206, 21)
+        Me.ComboBox5.Sorted = True
+        Me.ComboBox5.TabIndex = 12
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(31, 191)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(54, 13)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "NOMBRE"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(296, 191)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(26, 13)
+        Me.Label3.TabIndex = 14
+        Me.Label3.Text = "DNI"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(554, 191)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(64, 13)
+        Me.Label4.TabIndex = 15
+        Me.Label4.Text = "TELÉFONO"
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(597, 320)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(166, 23)
+        Me.Button8.TabIndex = 16
+        Me.Button8.Text = "INDICA TAMAÑO Y POSICIÓN"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'ComboBox6
+        '
+        Me.ComboBox6.FormattingEnabled = True
+        Me.ComboBox6.Location = New System.Drawing.Point(350, 322)
+        Me.ComboBox6.Name = "ComboBox6"
+        Me.ComboBox6.Size = New System.Drawing.Size(206, 21)
+        Me.ComboBox6.Sorted = True
+        Me.ComboBox6.TabIndex = 17
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ComboBox6)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ComboBox5)
+        Me.Controls.Add(Me.ComboBox4)
+        Me.Controls.Add(Me.ComboBox3)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Button6 As Button
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents Button7 As Button
+    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents ComboBox4 As ComboBox
+    Friend WithEvents ComboBox5 As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button8 As Button
+    Friend WithEvents ComboBox6 As ComboBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,145 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim RESPUESTA As Integer
+        Dim C As Integer
+        'HAZLO Y REPITE HASTA A C= C+1
+        Do
+            C += 1 'SERIA IGUAL A C= C+1
+            RESPUESTA = MsgBox(" LO HACEMOS OTRA VEZ Y VAN YA " & C, vbYesNo)
+        Loop Until RESPUESTA <> vbYes
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim RESPUESTA As Integer
+        Dim C As Integer
+        'HAZLO MIENTRAS
+        Do While RESPUESTA <> vbNo
+            C += 1 'SERIA IGUAL A C= C+1
+            RESPUESTA = MsgBox(" LO HACEMOS OTRA VEZ Y VAN YA " & C, vbYesNo)
+        Loop
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim RESPUESTA As Integer
+        Dim C As Integer
+        'MIENTRAS QUE HAZLO
+        While RESPUESTA <> vbNo
+            C += 1 'SERIA IGUAL A C= C+1
+            RESPUESTA = MsgBox(" LO HACEMOS OTRA VEZ Y VAN YA " & C, vbYesNo)
+        End While
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim NUMERO As Integer
+        Dim RESTO = 1
+        'HAZLO Y REPITE HASTA C= C+1
+        While RESTO <> 0
+            NUMERO = InputBox("METE UN NUMERO")
+            RESTO = NUMERO Mod 2
+        End While
+      
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim VECTOR(10) As String
+        For I = 0 To 10
+            VECTOR(I) = InputBox("METE JUGADOR" & I)
+        Next
+        For I = 0 To 10
+            ComboBox1.Items.Add(VECTOR(I))
+        Next
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Dim N As Integer 'CUANTOS VOY A ALMACENAR EN EL VECTOR
+        Dim NS As Integer  'Nº SUPERIOR AL AZAR
+        Dim NI As Integer   'Nº INFERIOR AL AZAR
+        N = InputBox("CUANTOS NUMEROS AL AZAR ALMACENAMOS", "CUANTOS NUMEROS AL AZAR ALMACENAMOS")
+
+
+        Dim VECTOR(N) As Integer
+        While NI >= NS 'MIENTRAS QUE INFERIOR SEA 0 O MAYOR QUE SUPERIOR
+            NS = InputBox("NUMERO AL AZAR", "AZAR SUPERIOR")
+        NI = InputBox("NUMERO AL AZAR INFERIOR", "AZAR INFERIOR")
+            If NI >= NS Then MsgBox("ERROR, HAS METIDO EN NUMERO INFERIOR UN Nº MAYOR O IGUAL")
+        End While
+
+        For I = 1 To N
+            VECTOR(I) = Int(Rnd() * (NS - NI) + NI)
+        Next
+
+        For I = 1 To N
+            ComboBox2.Items.Add(VECTOR(I))
+        Next
+
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        Dim LISTA(5, 3)
+        For I = 1 To 5
+            LISTA(I, 1) = InputBox("Mete Nombre")
+            LISTA(I, 2) = InputBox("Mete DNI")
+            LISTA(I, 3) = InputBox("Mete TELÉFONO")
+        Next
+
+        For I = 1 To 5
+            ComboBox3.Items.Add(LISTA(I, 1))
+            ComboBox4.Items.Add(LISTA(I, 2))
+            ComboBox5.Items.Add(LISTA(I, 3))
+        Next
+
+    End Sub
+
+    Private Sub ComboBox5_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox5.SelectedIndexChanged
+        'AL SELECCIONAR UN ELEMENTO DEL ÍNDICE DE COMBOBOX5, TE MUESTRA SUS IGUALES EN COMBOS 3 Y 4
+        ComboBox4.SelectedIndex = ComboBox5.SelectedIndex
+        ComboBox3.SelectedIndex = ComboBox5.SelectedIndex
+    End Sub
+
+    Private Sub ComboBox4_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox4.SelectedIndexChanged
+        'AL SELECCIONAR UN ELEMENTO DEL ÍNDICE DE COMBOBOX4, TE MUESTRA SUS IGUALES EN COMBOS 5 Y 3
+        ComboBox5.SelectedIndex = ComboBox4.SelectedIndex
+        ComboBox3.SelectedIndex = ComboBox4.SelectedIndex
+    End Sub
+
+    Private Sub ComboBox3_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox3.SelectedIndexChanged
+        'AL SELECCIONAR UN ELEMENTO DEL ÍNDICE DE COMBOBOX3, TE MUESTRA SUS IGUALES EN COMBOS 4 Y 5
+        ComboBox4.SelectedIndex = ComboBox3.SelectedIndex
+        ComboBox5.SelectedIndex = ComboBox3.SelectedIndex
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Randomize()
+        Dim VECTOR(500)
+        Dim NMAXIMO, NMINIMO, POSIMAXIMO, POSIMINIMO As Integer
+        NMAXIMO = 0
+        NMINIMO = 1000
+        'RELLENO EL VECTOR
+        For I = 1 To 500
+            VECTOR(I) = Int((Rnd() * 500) + 200)
+            ComboBox6.Items.Add(VECTOR(I))
+        Next
+
+        'BUSCAR EL MAYOR Y EL MENOR Y SUS POSICIONES (POSCICIÓN I)
+        For I = 1 To 500
+            If VECTOR(I) > NMAXIMO Then
+                NMAXIMO = VECTOR(I)
+                POSIMAXIMO = I
+            End If
+            If VECTOR(I) < NMINIMO Then
+                NMINIMO = VECTOR(I)
+                POSIMINIMO = I
+            End If
+        Next
+        'MUESTRA LOS RESULTADOS
+        MsgBox(" EL MAS GRANDE ES " & NMAXIMO)
+        MsgBox(" EL MAS PEQUEÑO ES " & NMINIMO)
+        MsgBox(" LA POSICION DEL GRANDE ES " & POSIMAXIMO)
+        MsgBox(" LA POSICION DEL GRANDE ES " & POSIMINIMO)
+
+    End Sub
+
+    Private Sub ComboBox6_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox6.SelectedIndexChanged
+
+    End Sub
+End Class
